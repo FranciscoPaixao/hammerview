113716b4e0e2882bd9bb40efddf770c4ed5eae17
splytech <zangzi@gmail.com>
2013-07-03 06:50:44 -0400

    Using less-middleware to compile the bootstrap css. This will hopefully allow me to load only the modules I will eventually need to minimize the load.

--- app.js
+++ app.js
@@ -7,20 +7,29 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , io_base = require('socket.io')
   , path = require('path');
+var less = require('less-middleware');
 
 var app = express();
+
+var bootstrapPath = path.join(__dirname, 'node_modules', 'bootstrap');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
-app.use(express.favicon());
+app.use(express.favicon('public/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(less({
+  src    : path.join(__dirname, 'assets', 'less'),
+  paths  : [path.join(bootstrapPath, 'less')],
+  dest   : path.join(__dirname, 'public', 'css'),
+  prefix : '/css'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -31,6 +40,16 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+var io = io_base.listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
